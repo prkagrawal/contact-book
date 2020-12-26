e237d25126ca24dbaacbce47f97e019966598c21
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-26 14:03:28 +0530

    user authentication setup completed

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const app = express();
+const morgan = require('morgan')
 
 // Connect Database
 const connectDb = require('./config/MongoDb')
 connectDb()
+
+// use middlewares
+app.use(express.json({ extended: false }));
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to the ContactKeeper API...' })
 })
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/auth', require('./routes/auth'));
+app.use('/api/v1/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
