c67390aed38fd1b8be78202647c6ea9a19e70956
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-26 17:37:22 +0530

    ContactState initial state and provider setup and added types

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+import ContactState from './context/contact/contactState';
 import './App.css'
 
 const App = () => {
   return (
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </React.Fragment>
+    <ContactState>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </React.Fragment>
     </Router>
+    </ContactState>
+    
   )
 }
 
