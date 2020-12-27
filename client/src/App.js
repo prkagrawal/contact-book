1fd60717e299570cc0962e793614edf9d87ae308
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-27 21:50:03 +0530

    AuthState initial state and provider setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,28 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/contactState';
 import './App.css'
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </React.Fragment>
-    </Router>
-    </ContactState>
-    
-  )
-}
+    <AuthState>
+      <ContactState>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
+  );
+};
 
 export default App;
