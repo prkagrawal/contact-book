8fcb9f0f98cd8e353d99c03c96e03055e9a3c78c
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-28 10:47:33 +0530

    Alert state, context and component setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,30 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
+import Alerts from './components/layout/Alerts';
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/contactState';
+import AlertState from './context/alert/AlertState';
 import './App.css'
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <React.Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-              </Switch>
-            </div>
-          </React.Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <React.Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                </Switch>
+              </div>
+            </React.Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
