9f241fdcb7eefdbee28b5f6a93dc3f4bf8a5fc99
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-29 11:21:35 +0530

    user Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Login = () => {
+const Login = props => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -14,7 +33,11 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Login submit");
+    if (email === '' || password === '') {
+      setAlert('Please fill in the required fields');
+    } else {
+      login({ email, password });
+    }
   };
 
   return (
@@ -31,6 +54,7 @@ const Login = () => {
             placeholder="example@mail.com"
             value={email}
             onChange={onChange}
+            required
           />
         </div>
         <div className="form-group">
@@ -40,6 +64,7 @@ const Login = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
           />
         </div>
         <input
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -72,7 +72,30 @@ const AuthState = props => {
     };
 
     // Login user
-    const login = () => console.log('login');
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.post('api/v1/auth', formData, config);
+
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            });
+
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err
+                // payload: err.response.data.msg
+            });
+        }
+    }
 
     // Logout
     const logout = () => console.log('logout');
