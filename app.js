1bae21a078ed2ddc115a9d645096f70eae72ede0
Prince Agrawal <princeagrawalnew123@gmail.com>
2021-01-05 21:39:22 +0530

    prepare for deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 // Connect Database
 const connectDb = require("./config/MongoDb");
@@ -21,6 +22,16 @@ app.get("/", (req, res) => {
 app.use("/api/v1/users", require("./routes/users"));
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
